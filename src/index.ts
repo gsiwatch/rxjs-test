6eb70081af6c90f4bffaec1ab61df200fe2adffc
gsiwatch <gaurish.siwatch@gmail.com>
2020-10-29 03:19:26 -0400

    bug fixes of initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
 import { Observable } from 'rxjs';
 
-const observable$ = new Observable((subscriber) => {
+const observable$ = new Observable(subscriber => {
     subscriber.next(1);
     subscriber.next(2); // emit 2
     subscriber.next(3); // emit 3
     setTimeout(() => {
-        subscriber.next(4);
+        subscriber.next(4); // emit 4
         subscriber.complete();
-    }, 1000);
-    subscriber.complete()
+    }, 2000);
 });
 
 console.log('just before subscriber');
 observable$.subscribe({
     next(x) { console.log('value is', x)},
     error(err: any) { console.log('error is', err)},
-    complete() {console.log('I am done')},
+    complete() { console.log('I am done') },
 });
 console.log('just after subscription');
 
